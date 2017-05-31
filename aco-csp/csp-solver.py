93e1f2ba498e67906010e6436a4b7105aff156cb
Fabio Navarrete <fabioanh@gmail.com>
2017-05-31 21:43:45 +0200

    Added instances to repository intended for testing purposes. First sketch of required structure for problem solution.

---
+++ aco-csp/csp-solver.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""Closest String Problem solution."""
+from . import Problem
+
+
+class CSPProblem(Problem):
+    """Closest String Problem Representation."""
+
+    def load_input_data(self, instance_location):
+        """Load input data from provided instances format."""
+        with open(instance_location) as file:
+            for i, line in enumerate(file):
+                if i == 0:
+
+                elif i == 1:
+        pass
