aef6499aca06905f93ea6343378e7042f9768cd9
Fabio Navarrete <fabioanh@gmail.com>
2017-05-31 22:08:41 +0200

    Added logic to load the instance files for the CSP

--- aco-csp/csp-solver.py
+++ aco-csp/csp-solver.py
@@ -6,11 +6,26 @@ from . import Problem
 class CSPProblem(Problem):
     """Closest String Problem Representation."""
 
+    def __init__(self, instance_location):
+        self.strings = []
+        self.alphabet = []
+        super(CSPProblem, self).__init__(instance_location)
+
     def load_input_data(self, instance_location):
         """Load input data from provided instances format."""
         with open(instance_location) as file:
             for i, line in enumerate(file):
                 if i == 0:
-
+                    self.alphabet_length = int(line.strip())
                 elif i == 1:
-        pass
+                    self.num_str = int(line.strip())
+                elif i == 2:
+                    self.str_length = int(line.strip())
+                elif i <= self.alphabet_length + 2:
+                    self.alphabet.append(line.strip())
+                elif line.strip():
+                    self.strings.append(line.strip())
+
+
+prob = CSPProblem('../instances/2-30-10000-1-9.csp')
+print(prob.alphabet_length)
