e8333e580b85e7c3b9727ec26db84b8af8e61267
Fabio Navarrete <fabioanh@gmail.com>
2017-06-03 10:51:09 +0200

    Added seed parameter to ensure consistency in random numbers generation

--- aco-csp/__main__.py
+++ aco-csp/__main__.py
@@ -11,12 +11,14 @@ Options:
   -b --beta=<val>       Beta value to be used
   -r --rho=<val>        Rho value to be used
   -n --numants=<val>    Number of ants [default: 10]
+  -s --seed=<val>       Seed to use in the random generator [default: 1234]
 """
 from docopt import docopt
 from csp_solver import CSPSolver
+import numpy as np
 
 if __name__ == '__main__':
     args = docopt(__doc__, version='FIXME')
+    np.random.seed(int(args['--seed']))
     solver = CSPSolver(args)
-    print(solver.problem.inv_alphabet)
     solver.solve()
